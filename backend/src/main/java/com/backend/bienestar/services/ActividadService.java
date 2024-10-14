bfdcd41f3fdbc246130b5f9231b1cecb35e134c9
Tomas Corbalan Mendelek <143723168+TomasMendelek@users.noreply.github.com>
2024-10-14 18:26:35 -0300

    Update ActividadService.java

--- backend/src/main/java/com/backend/bienestar/services/ActividadService.java
+++ backend/src/main/java/com/backend/bienestar/services/ActividadService.java
@@ -1,33 +1,37 @@
-package com.backend.bienestar.services;
-
-import java.util.List;
-import java.util.Optional;
-
+import com.backend.bienestar.models.Actividad;
+import com.backend.bienestar.repositories.ActividadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.backend.bienestar.models.Actividad;
-import com.backend.bienestar.repositories.ActividadRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ActividadService {
+
     @Autowired
     private ActividadRepository actividadRepository;
 
-    public List<Actividad> obtenerTodasLasActividades(){
+    public List<Actividad> obtenerTodasLasActividades() {
         return actividadRepository.findAll();
     }
+
     public Actividad crearActividad(Actividad actividad) {
         return actividadRepository.save(actividad);
     }
+
     public Optional<Actividad> obtenerActividadPorId(String id) {
         return actividadRepository.findById(id);
     }
+
+    public List<Actividad> obtenerActividadesPorInstructor(String instructorId) {
+        return actividadRepository.findAll().stream()
+                .filter(actividad -> actividad.getInstructorId().equals(instructorId))
+                .collect(Collectors.toList());
+    }
+
     public void eliminarActividad(String id) {
         actividadRepository.deleteById(id);
     }
-
-
-
-
 }
